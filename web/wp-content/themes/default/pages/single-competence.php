448f046cc38684dc31dc474befaa0addc4b39d06
Bryan VIDAL <b.vidal@LON-0438.local>
2022-12-22 23:44:47 +0100

    dev template single comp

---
+++ web/wp-content/themes/default/pages/single-competence.php
@@ -0,0 +1,196 @@
+<?php
+/* Template Name: Single Compétence */
+get_header();
+?>
+<div class="hero-single-competence container-gutters">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <a href="" class="button">
+                    retour
+                </a>
+            </div>
+            <div class="col-sm-8 mx-auto">
+                <div class="title-h1 text-center">
+                    Famille<br/>
+                    <i>Internationale</i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="strate-image-full">
+    <img src="<?= get_template_directory_uri(); ?>/medias/21.jpg" alt="">
+</div>
+
+<div class="strate-titre-center container-gutters">
+    <div class="row">
+        <div class="col-sm-6 mx-auto">
+            <div class="introduction-section">
+                Notre objectif
+            </div>
+
+            <div class="title-h4">
+                La mobilité croissante des individus les conduit à engager des relations présentant des éléments d’extranéité tels que leur nationalité, le lieu de célébration de leur mariage, le lieu de naissance de leur enfant, ou encore leur lieu de résidence ou de décès.
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="strate-image-text container-gutters">
+    <div class="row">
+        <div class="col-sm-9 mx-auto">
+            <div class="row">
+                <div class="col-sm-7">
+                    <div class="container-image">
+                        <img src="<?= get_template_directory_uri(); ?>/medias/22.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-sm-5">
+                    <div class="container-text">
+                        <p>
+                            Le droit international privé de la famille a pour objet essentiel d’apporter des outils lorsque la situation présente un ou plusieurs éléments d’extranéité et qu’il convient de déterminer le juge compétent et la loi applicable à votre différend. Nous pouvons également vous conseiller ou vous assister pour des questions de reconnaissance ou d’exécution d’une décision judiciaire dans l’espace international. Les praticiens du droit doivent aujourd’hui s’adapter à des problématiques juridiques autrefois essentiellement internes, et qui désormais s’inscrivent dans une sphère internationale. Le droit de la famille personnel et patrimonial, ainsi que le droit des successions sont ainsi confrontés à des questions complexes nécessitant une expertise approfondie en droit international privé pour déterminer une stratégie juridique qui vous serait la plus favorable ou la moins préjudiciable, ainsi que la maitrise de langues étrangères.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="strate-titre-center container-gutters">
+    <div class="row">
+        <div class="col-sm-6 mx-auto">
+            <div class="title-h4">
+                Notre expertise approfondie de ces matières
+                nous permet de vous accompagner et vous conseiller au quotidien pour définir une stratégie juridique et patrimoniale la mieux adaptée à votre situation personnelle en présence d’éléments d’extranéité.
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="strate-text-left container-gutters">
+    <div class="row">
+        <div class="col-sm-6 mx-auto">
+            <div class="row">
+
+                <div class="col-sm-6">
+                    <div class="container-text">
+                        <p>
+                            A cette fin, nous exerçons en anglais, en espagnol, en slovaque (tchèque) mais également en portugais, et travaillons conjointement avec des avocats d’autres pays spécialisés en droit de la famille. Nous disposons de ce fait d’un réseau compétent et réactif en Europe, et dans le monde entier.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="strate-slider-competences">
+
+    <div class="container-items-competences">
+    </div>
+    <section class="slider-section">
+        <div class="introduction-section">
+            Voir aussi
+        </div>
+        <div class="scroll-X">
+            <div class="card-slider-section ">
+                <img src="<?= get_template_directory_uri(); ?>/medias/08.jpg" alt="">
+                <div class="content-card">
+                    <div class="title-h4">
+                        Couple marié et non marié
+                    </div>
+                    <p class="discover-card">
+                        DÉCOUVRIR
+                    </p>
+                </div>
+            </div>
+            <div class="card-slider-section ">
+                <img src="<?= get_template_directory_uri(); ?>/medias/09.jpg" alt="">
+                <div class="content-card">
+                    <div class="title-h4">
+                        Conseil et anticipation patrimoniale
+                    </div>
+                    <p class="discover-card">
+                        DÉCOUVRIR
+                    </p>
+                </div>
+            </div>
+            <div class="card-slider-section ">
+                <img src="<?= get_template_directory_uri(); ?>/medias/10.jpg" alt="">
+                <div class="content-card">
+                    <div class="title-h4">
+                        Famille internationale
+                    </div>
+                    <p class="discover-card">
+                        DÉCOUVRIR
+                    </p>
+                </div>
+            </div>
+            <div class="card-slider-section ">
+                <img src="<?= get_template_directory_uri(); ?>/medias/11.jpg" alt="">
+                <div class="content-card">
+                    <div class="title-h4">
+                        Famille internationale
+                    </div>
+                    <p class="discover-card">
+                        DÉCOUVRIR
+                    </p>
+                </div>
+            </div>
+            <div class="card-slider-section ">
+                <img src="<?= get_template_directory_uri(); ?>/medias/08.jpg" alt="">
+                <div class="content-card">
+                    <div class="title-h4">
+                        Couple marié et non marié
+                    </div>
+                    <p class="discover-card">
+                        DÉCOUVRIR
+                    </p>
+                </div>
+            </div>
+            <div class="card-slider-section ">
+                <img src="<?= get_template_directory_uri(); ?>/medias/09.jpg" alt="">
+                <div class="content-card">
+                    <div class="title-h4">
+                        Conseil et anticipation patrimoniale
+                    </div>
+                    <p class="discover-card">
+                        DÉCOUVRIR
+                    </p>
+                </div>
+            </div>
+            <div class="card-slider-section ">
+                <img src="<?= get_template_directory_uri(); ?>/medias/10.jpg" alt="">
+                <div class="content-card">
+                    <div class="title-h4">
+                        Famille internationale
+                    </div>
+                    <p class="discover-card">
+                        DÉCOUVRIR
+                    </p>
+                </div>
+            </div>
+            <div class="card-slider-section ">
+                <img src="<?= get_template_directory_uri(); ?>/medias/11.jpg" alt="">
+                <div class="content-card">
+                    <div class="title-h4">
+                        Famille internationale
+                    </div>
+                    <p class="discover-card">
+                        DÉCOUVRIR
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <a href="" class="button all-know-cta">
+            Tous nos savoir-faire
+        </a>
+    </section>
+</div>
+
+<?php
+get_footer();
